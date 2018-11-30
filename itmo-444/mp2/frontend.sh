77b510461f3a8a42958af8f9916e774cf7a4cf91
jknific719 <jknific719@gmail.com>
2018-11-30 11:27:25 -0600

    Renamed create-app.sh to frontend.sh; removed some db php

--- itmo-444/mp2/frontend.sh
+++ itmo-444/mp2/frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+# User-data for the frontend webhosting servers
 apt-get update
 apt-get install -y apache2
 apt-get install -y git
@@ -9,9 +9,6 @@ sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 sudo php composer-setup.php --quiet
 sudo php -d memory_limit=-1 composer.phar require aws/aws-sdk-php 1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
 git clone git@github.com:illinoistech-itm/jknific.git
-cp jknific/itmo-444/mp2/dbcreate.php /home/ubuntu
-sudo php dbcreate.php --quiet
-
 cp jknific/itmo-444/mp2/index.html /var/www/html/index.html
 
 systemctl start apache2
