6f4acb2d4c221e355c10516a4c5f86e62bbfbe22
jknific719 <jknific719@gmail.com>
2018-11-30 11:27:53 -0600

    Added backend.sh for setting up user-data on backend server

---
+++ itmo-444/mp2/backend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# User-data script for the backend image manipulation instance
+apt-get update
+apt-get install -y apache2
+apt-get install -y git
+sudo apt-get install -y curl php php-simplexml unzip zip libapache2-mod-php php-xml php-mysql awscli
+cd /home/ubuntu
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php composer-setup.php --quiet
+sudo php -d memory_limit=-1 composer.phar require aws/aws-sdk-php 1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
+git clone git@github.com:illinoistech-itm/jknific.git
+cp jknific/itmo-444/mp2/dbcreate.php /home/ubuntu
+sudo php dbcreate.php --quiet
+exit 0
