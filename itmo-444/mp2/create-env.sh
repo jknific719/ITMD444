f13a14842389695a7c4ae448271305f6bb47c9aa
jknific719 <jknific719@gmail.com>
2018-11-30 11:28:07 -0600

    Added run-instance for backend

--- itmo-444/mp2/create-env.sh
+++ itmo-444/mp2/create-env.sh
@@ -9,7 +9,9 @@ elb=$6
 
 # Create EC2 instances
 echo "Creating EC2 Instances"
-avilzone=($(aws ec2 run-instances --image-id $imgid --count $count --instance-type t2.micro --key-name $keyname --security-groups $secgrp --iam-instance-profile $iamprofile --user-data file://create-app.sh | grep  AvailabilityZone | awk '{ print $2 }' | tr -d '",[]'))
+echo "First creating backend..."
+aws ec2 run-instances --image-id $imgid --count 1 --instance-type t2.micro --key-name $keyname --security-groups $secgrp --iam-instance-profile $iamprofile --user-data file://backend.sh
+avilzone=($(aws ec2 run-instances --image-id $imgid --count $count --instance-type t2.micro --key-name $keyname --security-groups $secgrp --iam-instance-profile $iamprofile --user-data file://frontend.sh | grep  AvailabilityZone | awk '{ print $2 }' | tr -d '",[]'))
 instances=($(aws ec2 describe-instances --filters "Name=image-id,Values=$imgid" "Name=instance-state-name,Values=pending,running" --query 'Reservations[*].Instances[*].InstanceId' | awk '{ print $1 }' | tr -d '",[]'))
 echo "Waiting..."
 aws ec2 wait instance-running --instance-ids ${instances[0]} ${instances[1]} ${instances[2]}
