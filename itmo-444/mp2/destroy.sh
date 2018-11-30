074b671dd4616a198b04c31e0a22fbd7da01e0b4
jknific719 <jknific719@gmail.com>
2018-11-30 15:37:26 -0600

    Fixed destory.sh

--- itmo-444/mp2/destroy.sh
+++ itmo-444/mp2/destroy.sh
@@ -4,7 +4,7 @@ imgid=$1
 elb=$2
 db_id=$3
 instances=($(aws ec2 describe-instances --filters "Name=image-id,Values=$imgid" "Name=instance-state-name,Values=pending,running" --query 'Reservations[*].Instances[*].InstanceId' | awk '{ print $1 }' | tr -d '",[]'))
-aws ec2 terminate-instances --instance-ids ${instances[0]} ${instances[1]} ${instances[2]}
+aws ec2 terminate-instances --instance-ids ${instances[0]} ${instances[1]} ${instances[2] ${instances[3]}
 aws elb delete-load-balancer --load-balancer-name $elb
 aws s3 rb s3://mp2raw --force
 aws s3 rb s3://mp2finished --force
