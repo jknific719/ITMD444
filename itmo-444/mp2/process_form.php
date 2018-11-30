16b1f55ee25b0850de113f8a72a10d50d116e8bf
jknific719 <jknific719@gmail.com>
2018-11-30 16:22:23 -0600

    Combined process_form.php and db_add.php

--- itmo-444/mp2/process_form.php
+++ itmo-444/mp2/process_form.php
@@ -35,5 +35,24 @@
 		echo $e->getMessage();
 	}
   $s3url = "https://$bucket.s3.amazonaws.com/$keyName";
-  include 'db_add.php';
+  use Aws\Rds\RdsClient;
+  $rds = new Aws\Rds\RdsClient([
+       'version' => 'latest',
+       'region' => 'us-east-2'
+   ]);
+
+  $result = $rds->describeDBInstances([
+      'DBInstanceIdentifier' => 'mp2database'
+  ]);
+  $rdsIP = $result['DBInstances'][0]['Endpoint']['Address'];
+  $mysqli = mysqli_connect($rdsIP,"mrvl","excelsior","requestdata") or die("Error " . mysqli_error($mysqli));
+  # 0 = not done, 1 = done for status
+  $sql = "INSERT INTO requests
+    (name, email, phone, s3rawurl, status)
+    VALUES ($name, $email, $tel, $s3url, 0)";
+    if ($mysqli->query($sql)) {
+        printf("Values added to requests table.\n");
+    }
+  $mysqli->close();
+
 ?>
