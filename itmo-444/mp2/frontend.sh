a4bc3524ef8260ba58e2ebc250599f683b774979
jknific719 <jknific719@gmail.com>
2018-11-30 13:57:12 -0600

    cpy statement added

--- itmo-444/mp2/frontend.sh
+++ itmo-444/mp2/frontend.sh
@@ -10,7 +10,8 @@ sudo php composer-setup.php --quiet
 sudo php -d memory_limit=-1 composer.phar require aws/aws-sdk-php 1>> /home/ubuntu/out.log 2>> /home/ubuntu/err.log
 git clone git@github.com:illinoistech-itm/jknific.git
 cp jknific/itmo-444/mp2/index.html /var/www/html/index.html
-
+cp jknific/itmo-444/mp2/process_form.php /var/www/html/process_form.php
+cp jknific/itmo-444/mp2/db_add.php /var/www/html/db_add.php
 systemctl start apache2
 
 exit 0
